e1bcc4bb96287cc63f0dfd352c2f6e9249958c28
André Carvalho <andre.bonifacio@edu.pucrs.br>
2025-09-05 22:49:47 +0000

    pesquisa no mapa

--- src/app/gerenciamentoArea/components/GoogleMap/GoogleMap.tsx
+++ src/app/gerenciamentoArea/components/GoogleMap/GoogleMap.tsx
@@ -25,6 +25,7 @@ export default function GoogleMap({
     const loader = new Loader({
       apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
       version: 'weekly',
+      libraries: ['places']
     });
 
     loader
